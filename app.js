b109c8220dba4395bdb1446bc6d37f6adf312b01
Rakesh Patel <emailme@rakeshrpatel.com>
2016-08-22 16:18:45 -0400

    circle designs after click

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Adapt to Angular
 // Finish CSS styling
 
 var headers = $(".portfolioHeaders > h2").toArray();
@@ -7,33 +6,94 @@ var navigationCircles = $('.navigationCircles').children().toArray();
 var i = 0;
 
 $(function() {
+    setInterval(updateGradient, 10);
 
-    // $('#gradient').css({
-    //     background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
-    // }).css({
-    //     background: "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
-    // });
-    
-    // setInterval(updateGradient,10);
+    $('.fa-envelope-o').hover(function() {
+        $('.fa-linkedin').css('opacity', 1);
+        $('.fa-github').css('opacity', 1);
 
-    $('#topBars').click(function(){
-        $('.contactLinks').toggle()
     });
-    
+
+    $('.contactLinks').mouseout(function() {
+        $('.fa-linkedin').css('opacity', 0);
+        $('.fa-github').css('opacity', 0)
+    })
+
     $('.aboutMeLink').click(function() {
-        $('.aboutMeBlurb').slideToggle(800);
+        $("[id='pageNavigation']").hide();
+        $("[class='aboutMeClick']").show();
+        $('#fa-times').show();
+        // $('.aboutMeClick').wrapAll('display');
         $(headers).hide(800);
         $(images).hide(800);
         $('.portfolioNavigation').hide(800);
+        $('.aboutMeLink').parent()
+            .css({ 'border': '1px dotted #99e052' })
+            .css({ 'box-shadow': '1px 1px 3px 1px #52e0e0' })
+            .siblings()
+            .css({ 'border': '1px solid purple' })
+            .css({ 'box-shadow': '0px 0px 0px 0px' });
         i = 0;
 
     });
 
+    $('#fa-times').click(function() {
+        $("[class='aboutMeClick']").hide();
+        $('.aboutMeBlurb').children().hide();
+        $('#fa-times').hide();
+        $('#circle')
+            .css({ 'border': '1px solid purple' })
+            .css({ 'box-shadow': '0px 0px 0px 0px' });
+        // $('.pageNavigation').children()
+        //     .css({ 'border': '1px solid purple' });
+            // .css({ 'box-shadow': '0px 0px 0px 0px' });
+        $("[id='pageNavigation']").show();
+    })
+
+    $('#whoAmI').click(function() {
+        $('.aboutMePartI').show().siblings().hide();
+        $('#whoAmI')
+            .parent()
+            .css({ 'border': '1px dotted #99e052' })
+            .css({ 'box-shadow': '1px 1px 3px 1px #52e0e0' })
+            .siblings()
+            .css({ 'border': '1px solid purple' })
+            .css({ 'box-shadow': '0px 0px 0px 0px' });
+    });
+
+    $('#whatMovesMe').click(function() {
+        $('.aboutMePartII').show().siblings().hide();
+        $('#whatMovesMe')
+            .parent()
+            .css({ 'border': '1px dotted #99e052' })
+            .css({ 'box-shadow': '1px 1px 3px 1px #52e0e0' })
+            .siblings()
+            .css({ 'border': '1px solid purple' })
+            .css({ 'box-shadow': '0px 0px 0px 0px' });
+    });
+
+    $('#otherTidBits').click(function() {
+        $('.aboutMePartIII').show().siblings().hide();
+        $('#otherTidBits')
+            .parent()
+            .css({ 'border': '1px dotted #99e052' })
+            .css({ 'box-shadow': '1px 1px 3px 1px #52e0e0' })
+            .siblings()
+            .css({ 'border': '1px solid purple' })
+            .css({ 'box-shadow': '0px 0px 0px 0px' });
+    });
+
     $('.portfolioLink').click(function() {
-        $('.aboutMeBlurb').slideUp(800);
+        $('.aboutMeBlurb').children().hide();
         $('.portfolioNavigation').slideToggle(800);
         $(images[i]).slideToggle(800);
         $(headers[i]).slideToggle(800);
+        $('.portfolioLink').parent()
+            .css({ 'border': '1px dotted #99e052' })
+            .css({ 'box-shadow': '1px 1px 3px 1px #52e0e0' })
+            .siblings()
+            .css({ 'border': '1px solid purple' })
+            .css({ 'box-shadow': '0px 0px 0px 0px' });
         $(navigationCircles[i]).css('color', 'gold').siblings().css('color', 'white');
         i = 0;
 
@@ -68,51 +128,57 @@ $(function() {
 
 });
 
-// GRADIENT COLOR EFFECT:
-// var colors = new Array(
-//     [62, 35, 255], [60, 255, 60], [255, 35, 98], [45, 175, 230], [255, 0, 255], [255, 128, 0]);
 
-// var step = 0;
+var colors = new Array(
+    [62, 35, 255], [60, 255, 60], [255, 35, 98], [45, 175, 230], [255, 0, 255], [255, 128, 0]);
+
+var step = 0;
 //color table indices for: 
 // current color left
 // next color left
 // current color right
 // next color right
-// var colorIndices = [0, 1, 2, 3];
+var colorIndices = [0, 1, 2, 3];
 
 //transition speed
-// var gradientSpeed = 0.002;
+var gradientSpeed = 0.002;
 
-// function updateGradient() {
+function updateGradient() {
 
-//     if ($ === undefined) return;
+    if ($ === undefined) return;
 
-//     var c0_0 = colors[colorIndices[0]];
-//     var c0_1 = colors[colorIndices[1]];
-//     var c1_0 = colors[colorIndices[2]];
-//     var c1_1 = colors[colorIndices[3]];
+    var c0_0 = colors[colorIndices[0]];
+    var c0_1 = colors[colorIndices[1]];
+    var c1_0 = colors[colorIndices[2]];
+    var c1_1 = colors[colorIndices[3]];
 
-//     var istep = 1 - step;
-//     var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-//     var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-//     var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-//     var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+    var istep = 1 - step;
+    var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+    var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+    var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+    var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
 
-//     var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-//     var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-//     var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-//     var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+    var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+    var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+    var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+    var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
 
-//     step += gradientSpeed;
-//     if (step >= 1) {
-//         step %= 1;
-//         colorIndices[0] = colorIndices[1];
-//         colorIndices[2] = colorIndices[3];
+    $('#gradient').css({
+            "background": "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
+        })
+        // .css({
+        //    'background-color', "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
 
-//         //pick two new target color indices
-//         //do not pick the same as the current one
-//         colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-//         colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+    step += gradientSpeed;
+    if (step >= 1) {
+        step %= 1;
+        colorIndices[0] = colorIndices[1];
+        colorIndices[2] = colorIndices[3];
 
-//     };
-// };
+        //pick two new target color indices
+        //do not pick the same as the current one
+        colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+        colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+
+    }
+}
